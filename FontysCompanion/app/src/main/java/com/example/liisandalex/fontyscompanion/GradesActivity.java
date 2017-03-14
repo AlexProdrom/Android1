703d6e7deea43184242d83e821480b97c35c2c72
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2017-03-14 23:32:46 +0100

    Modifications regarding Companion and the camera app

---
+++ FontysCompanion/app/src/main/java/com/example/liisandalex/fontyscompanion/GradesActivity.java
@@ -0,0 +1,23 @@
+package com.example.liisandalex.fontyscompanion;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GradesActivity extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.activity_grades,container,false);
+        return view;
+    }
+}
--- FontysCompanion/app/src/main/java/com/example/liisandalex/fontyscompanion/MainActivity.java
+++ FontysCompanion/app/src/main/java/com/example/liisandalex/fontyscompanion/MainActivity.java
@@ -1,33 +1,42 @@
 package com.example.liisandalex.fontyscompanion;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    private FragmentTransaction transaction;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_schedule);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_grades);
-                    return true;
+                    selectedFragment = new HomeActivity();
+                    break;
+                case R.id.navigation_schedule:
+                    selectedFragment = new ScheduleActivity();
+                    break;
+                case R.id.navigation_grades:
+                    selectedFragment = new GradesActivity();
+                    break;
             }
-            return false;
+
+            transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.content, selectedFragment);
+            transaction.commit();
+            return true;
         }
 
     };
@@ -37,9 +46,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        HomeActivity firstpage=new HomeActivity();
+        transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.content,firstpage);
+        transaction.commit();
     }
 
 }
